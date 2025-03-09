c1d198374e04beedab7b4d755535659c55d05258
rebeccapeltz <rebeccapeltz@gmail.com>
2025-03-09 16:53:45 -0700

    cleanup tts JavaScript

--- static/js/tts.js
+++ static/js/tts.js
@@ -41,30 +41,6 @@ function getVoice(countryCode) {
 }
 
 
-const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-
-function base64ToArrayBuffer(base64) {
-    const binaryString = atob(base64);
-    const len = binaryString.length;
-    const bytes = new Uint8Array(len);
-    for (let i = 0; i < len; i++) {
-        bytes[i] = binaryString.charCodeAt(i);
-    }
-    return bytes.buffer;
-}
-
-function playAudioFromBase64(base64Data) {
-    const arrayBuffer = base64ToArrayBuffer(base64Data);
-    audioContext.decodeAudioData(arrayBuffer, (buffer) => {
-        const source = audioContext.createBufferSource();
-        source.buffer = buffer;
-        source.connect(audioContext.destination);
-        source.start(0);
-    }, (error) => {
-        console.error('Error decoding audio data:', error);
-    });
-}
-
 function playAudioFromResponse(response) {
   response.blob().then(blob => {
       const audioUrl = URL.createObjectURL(blob);
