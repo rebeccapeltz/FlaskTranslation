5093f685f675d44a328a227a9010a17095a5a6f7
rebeccapeltz <rebeccapeltz@gmail.com>
2025-03-12 20:53:19 -0700

    add about;add navbar;resizing

--- app.py
+++ app.py
@@ -37,6 +37,9 @@ def inject_now():
 # def index_get():
 #     return render_template('index.html')
 
+@app.route("/about", methods=["GET"])
+def about():
+    return render_template('about.html')
 
 @app.route("/", methods=["GET", "POST"])
 def index():
